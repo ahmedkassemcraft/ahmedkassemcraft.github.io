22b96c0c60cbdb851ec708cd000f722497a45c31
ahmedkassemcraft <74984657+ahmedkassemcraft@users.noreply.github.com>
2023-05-10 19:51:40 -0500

    Update noticias.js
    Signed-off-by: ahmedkassemcraft <74984657+ahmedkassemcraft@users.noreply.github.com>

--- noticias.js
+++ noticias.js
@@ -1,18 +1,1 @@
-// Función para agregar un nuevo artículo a la lista
-function agregarArticulo(titulo, autor, fecha, contenido, imagenURL) {
-  var listaArticulos = document.getElementById("lista");
 
-  var nuevoArticulo = document.createElement("li");
-  nuevoArticulo.innerHTML = "<h3>" + titulo + "</h3>" +
-    "<p>Autor: " + autor + "</p>" +
-    "<p>Fecha: " + fecha + "</p>" +
-    "<p>" + contenido + "</p>";
-
-  if (imagenURL) {
-    var imagen = document.createElement("img");
-    imagen.src = imagenURL;
-    nuevoArticulo.appendChild(imagen);
-  }
-
-  listaArticulos.appendChild(nuevoArticulo);
-}
